44135b90510fa26aad74d98fecf03922316380cc
Fabien Chavonet <fchavonet@mail.fr>
2025-11-28 10:32:43 +0100

    Make our data from Password class mutable.

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,63 @@
+class Password {
+  String _password = "";
+
+  Password({required String password}) {
+    _password = password;
+  }
+
+  // Getter.
+  String get password {
+    return _password;
+  }
+
+  // Setter.
+  set password(String newPassword) {
+    _password = newPassword;
+  }
+
+  bool isValid() {
+    if (_password.length < 8) {
+      return false;
+    }
+    if (_password.length > 16) {
+      return false;
+    }
+
+    bool hasUpper = false;
+    bool hasLower = false;
+    bool hasDigit = false;
+
+    for (int i = 0; i < _password.length; i++) {
+      final char = _password[i];
+
+      if (char.toUpperCase() == char && char.toLowerCase() != char) {
+        hasUpper = true;
+      }
+
+      if (char.toLowerCase() == char && char.toUpperCase() != char) {
+        hasLower = true;
+      }
+
+      if (int.tryParse(char) != null) {
+        hasDigit = true;
+      }
+    }
+
+    if (hasUpper == false) {
+      return false;
+    }
+    if (hasLower == false) {
+      return false;
+    }
+    if (hasDigit == false) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
